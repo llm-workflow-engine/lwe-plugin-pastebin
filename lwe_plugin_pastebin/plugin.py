f44e001a072a9a338a33fc0d8f661b2e5d0fd829
Chad Phillips <chad@apartmentlines.com>
2023-07-16 15:33:59 -0400

    make browser backend incompatible

--- lwe_plugin_pastebin/plugin.py
+++ lwe_plugin_pastebin/plugin.py
@@ -29,6 +29,11 @@ class Pastebin(Plugin):
     Post a conversation to https://pastebin.com
     """
 
+    def incompatible_backends(self):
+        return [
+            'browser',
+        ]
+
     def default_config(self):
         return {
             'paste_defaults': {
